bbfcfdc17b41f0783a6a37ac8092d16d0af588e4
NeoKaios <mdgscode@mailo.com>
2024-03-14 17:29:26 +0100

    revert(front): playlists handled by App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import "./App.scss";
 import { PlaylistList } from "./components";
 import { getAccessToken, removeCookie } from "./lib/cookie";
 import { ACCESS_TOKEN_COOKIE, REFRESH_TOKEN_COOKIE } from "./lib/consts";
+import { useState } from "react";
+import { SimplifiedPlaylist } from "spotify-types";
+import { fetchPlaylists } from "./lib/fetchPlaylists";
 
 function logout() {
   removeCookie(ACCESS_TOKEN_COOKIE);
@@ -10,8 +13,18 @@ function logout() {
 }
 
 function App() {
+  const [playlists, setPlaylists] = useState<SimplifiedPlaylist[]>();
+
   const access_token = getAccessToken();
   const logged = access_token !== undefined;
+
+  const getPlaylistsAsync = async (access_token: string) => {
+    setPlaylists(await fetchPlaylists(access_token));
+  };
+
+  if (logged && !playlists) {
+    getPlaylistsAsync(access_token);
+  }
 
   return (
     <div className="App">
@@ -22,7 +35,7 @@ function App() {
         ) : (
           <a href="http://localhost:4000/login">Login</a>
         )}
-        {logged ? <PlaylistList></PlaylistList> : null}
+        {logged && playlists ? <PlaylistList playlists={playlists}></PlaylistList> : null}
       </header>
     </div>
   );
--- frontend/src/components/playlist-list/PlaylistList.tsx
+++ frontend/src/components/playlist-list/PlaylistList.tsx
@@ -1,26 +1,8 @@
-import { useState } from "react";
 import { SimplifiedPlaylist } from "spotify-types";
 import { PlaylistElement } from "..";
-import { getAccessToken } from "../../lib/cookie";
-import { fetchPlaylists } from "../../lib/fetchPlaylists";
 import './playlist-list.scss';
 
-export function PlaylistList() {
-  const [playlists, setPlaylists] = useState<SimplifiedPlaylist[]>();
-
-  const getPlaylistsAsync = async (access_token: string) => {
-    setPlaylists(await fetchPlaylists(access_token));
-  };
-
-  const access_token = getAccessToken();
-
-  if (access_token === undefined) {
-    return null;
-  }
-  else if (playlists == null) {
-    getPlaylistsAsync(access_token);
-    return null;
-  }
+export function PlaylistList({ playlists }: { playlists: SimplifiedPlaylist[] }) {
   return <div className="playlist-li">
     {playlists.map((playlist) => {
       return <PlaylistElement playlist={playlist}></PlaylistElement>
