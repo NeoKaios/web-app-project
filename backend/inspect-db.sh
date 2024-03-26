37f1a1a725b5c3426dbdde8cf1cc78719ec1dc5e
NeoKaios <mdgscode@mailo.com>
2024-03-26 19:23:43 +0100

    feat(back): improved inspect-db script

--- backend/inspect-db.sh
+++ backend/inspect-db.sh
@@ -1,2 +1,6 @@
+if [ -f .env ]
+then
+  export $(cat .env | grep -v ^# | xargs)
+fi
 DB_IP=$(docker container inspect db-dev | grep -Po "(?<=\"IPAddress\": \")[0-9].*[0-9]")
-mysql -u root -p -P 3306 -h $DB_IP -D blindtest-db
+mysql -u root -p$MYSQL_ROOT_PASSWORD -P $DB_PORT -h $DB_IP -D $MYSQL_DATABASE
