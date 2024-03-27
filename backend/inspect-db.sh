491c9beb6eebd8e0034b95bfc39d9db553a18db6
micronoyau <leo.lf.dk@gmail.com>
2024-03-27 19:15:15 +0100

    chore(config): Removed separate volume for dev npm

--- backend/inspect-db.sh
+++ backend/inspect-db.sh
@@ -3,4 +3,5 @@ then
   export $(cat .env | grep -v ^# | xargs)
 fi
 DB_IP=$(docker container inspect db-dev | grep -Po "(?<=\"IPAddress\": \")[0-9].*[0-9]")
+DB_PORT=3306
 mysql -u root -p$MYSQL_ROOT_PASSWORD -P $DB_PORT -h $DB_IP -D $MYSQL_DATABASE
