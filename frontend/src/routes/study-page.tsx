fcc28c0fd2e38c319458ea8216849b1a8044d50d
NeoKaios <mdgscode@mailo.com>
2024-03-27 10:31:52 +0100

    fix(front): remaining bugs in study-page

--- frontend/src/routes/study-page.tsx
+++ frontend/src/routes/study-page.tsx
@@ -47,7 +47,7 @@ export function StudyPage() {
     if (!updatedToStudy.length && !updatedNewTracks.length) {
       return;
     }
-    return toStudy.length ? randomChoice(updatedToStudy) : randomChoice(updatedNewTracks);
+    return updatedToStudy.length ? randomChoice(updatedToStudy) : randomChoice(updatedNewTracks);
   }
 
   useEffect(() => {
@@ -56,7 +56,7 @@ export function StudyPage() {
     }
   }, []);
 
-  if ((!newTracks && !toStudy) || !selectedTrack) {
+  if ((!newTracks.length && !toStudy.length) || !selectedTrack) {
     return <p>You already studied everything in this playlist !</p>;
   }
 
