62be293a6916adbf023559543237dc2c8f66f56a
NeoKaios <mdgscode@mailo.com>
2024-04-11 17:45:02 +0200

    fix(back): remove duplicate

--- backend/src/reqs.ts
+++ backend/src/reqs.ts
@@ -3,17 +3,6 @@ import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 import { sessions, CLIENT_SECRET } from "./consts";
 
-export function locallogin(req: Request, res: Response) {
-  if (req.query.userLogin) {
-    res.send(jwt.sign({ isAdmin: false }, CLIENT_SECRET));
-  } else if (req.query.password === 'admin') {
-    res.send({ success: true, token: jwt.sign({ isAdmin: true }, CLIENT_SECRET) });
-  } else {
-    res.send({ success: false, token: '' });
-  }
-  return res;
-}
-
 export function storeRequest(req: Request, res: Response) {
   const requestToAdd = req.query.req_url
   if (typeof requestToAdd === "string") {
