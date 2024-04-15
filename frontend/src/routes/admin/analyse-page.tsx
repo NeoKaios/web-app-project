a2c9ab4bf5922b69f45dd7960f0506cb28acf611
NeoKaios <mdgscode@mailo.com>
2024-04-15 13:24:16 +0200

    feat(front): change analyse to include all songs

--- frontend/src/routes/admin/analyse-page.tsx
+++ frontend/src/routes/admin/analyse-page.tsx
@@ -7,8 +7,7 @@ import { deleteExtra, deleteRequest, uploadAudioFiles } from '../../lib/backend-
 export async function analyseLoader({ params: { playlist_id } }: any) {
   console.log('Loading analyse page...');
   const playlist = await getPlaylist(playlist_id);
-  const songs = await getPlaylistItems(playlist_id);
-  const tracks = songs.filter((track) => !track.preview_url || track.preview_url.startsWith(BACK_URL));
+  const tracks = await getPlaylistItems(playlist_id);
   return { tracks, playlist };
 }
 
@@ -46,7 +45,8 @@ export function AnalysePage() {
           <tr>
             <th>Requested song ID</th>
             <th className='main'>Requested Song</th>
-            <th>Selected file</th>
+            <th>Audio</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -54,13 +54,11 @@ export function AnalysePage() {
             <tr key={id}>
               <td>{id}</td>
               <td>{name}</td>
-              <td className='file'>
-                {preview_url ?
-                  <>
-                    <audio controls src={preview_url}></audio>
-                    <button onClick={() => performAndRefresh(() => deleteExtra(id))}>Delete</button>
-                  </>
-                  : <input type="file" id={id} name="song" accept="audio/mpeg,audio/wav, audio/webm" />
+              <td> {preview_url ? <div style={{display:"flex", alignItems:"center"}}>{preview_url.startsWith(BACK_URL) ? "LOCAL" : "SPOTIFY"} <audio controls src={preview_url}></audio></div> : "No song present"}</td>
+              <td>
+                {(!preview_url || !preview_url.startsWith(BACK_URL)) ?
+                  <input type="file" id={id} name="song" accept="audio/mpeg,audio/wav, audio/webm" />
+                  : <button onClick={() => performAndRefresh(() => deleteExtra(id))}>Delete</button>
                 }
               </td>
             </tr>
